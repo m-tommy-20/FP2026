a68b107f5d68c2ce7d10eaeaa95a6293d793b72b
m-tommy-20 <mados.tamas@student.ms.sapientia.ro>
2026-03-12 11:38:51 +0200

    4. Labor

--- 02.labor/lab2_A.hs
+++ 02.labor/lab2_A.hs
@@ -1,3 +1,4 @@
+import Text.Parsec.Token (GenLanguageDef(reservedNames), GenTokenParser (natural))
 -- # 2. labor
 
 -- I. Könyvtárfüggvények használata nélkül, definiáljuk azt a függvényt, amely meghatározza:
@@ -16,15 +17,65 @@ szjSzorzat3 n res
   | div n 10 == 0 = res * n
   | otherwise = szjSzorzat3 (div n 10) (res * (mod n 10))
 -- - egy szám számjegyeinek összegét (2 módszerrel),
--- - egy szám számjegyeinek számát (2 módszerrel),
--- - egy szám azon számjegyeinek összegét, mely paraméterként van megadva, pl. legyen a függvény neve fugv4, ekkor a következő meghívásra, a következő eredményt kell kapjuk:
+szjOsszeg n
+  | n < 0 = szjOsszeg (abs n)
+  | n < 10 = n
+  | otherwise = mod n 10 + szjOsszeg (div n 10)
 
+szjOsszeg2 n res
+  | n < 0 = szjOsszeg2 (abs n) res
+  | n < 10 = res + n
+  | otherwise = szjOsszeg2 (div n 10) (res + mod n 10)
+-- - egy szám számjegyeinek számát (2 módszerrel),
+szjSzam n res
+  | n < 0 = szjSzam (abs n) res
+  | n < 10 = res + 1
+  | otherwise = szjSzam (div n 10) (res + 1)
+
+szjSzam2 n
+  | n < 0 = szjSzam2 (abs n)
+  | n < 10 = 1
+  | otherwise = 1 + szjSzam2 (div n 10)
+-- - egy szám azon számjegyeinek összegét, mely paraméterként van megadva, pl. legyen a függvény neve fugv4, ekkor a következő meghívásra, a következő eredményt kell kapjuk:
 --   ```haskell
 --   > fugv4 577723707 7
 --   35
 --   ```
+szjSzamOsszeg n szj
+  | szj > 9 = error "nem szj"
+  | n < 10 = if n == szj then szj else 0
+  | otherwise = if mod n 10 == szj then szj + szjSzamOsszeg (div n 10) szj else szjSzamOsszeg(div n 10) szj
+
+szjSzamOsszeg2 n szj elof
+  | szj > 9 = error "nem szj"
+  | n < 10 = if n == szj then (elof + 1) * szj else szj * elof
+  | otherwise = if mod n 10 == szj then szjSzamOsszeg2 (div n 10) szj (elof + 1) else szjSzamOsszeg2 (div n 10) szj elof
 -- - egy szám páros számjegyeinek számát,
+parosSzjSzam n res
+  | n < 0 = parosSzjSzam (abs n) res
+  | n < 10 = if even n then res + 1 else res
+  | otherwise =
+    if even (mod n 10)
+      then parosSzjSzam (div n 10) (res + 1)
+      else parosSzjSzam (div n 10) res
+  
+parosSzjSzam2 n
+  | n < 0 = parosSzjSzam2 (abs n)
+  | n < 10 = if even n then 1 else 0
+  | otherwise =
+    if even (mod n 10)
+      then parosSzjSzam2 (div n 10) + 1
+      else parosSzjSzam2 (div n 10)
 -- - egy szám legnagyobb számjegyét,
+
+lgSzj n ln
+  | n < 0 = lgSzj (abs n) ln
+  | n < 10 = max n ln
+  | otherwise = 
+    if mod n 10 > ln
+      then lgSzj (div n 10) (mod n 10)
+      else lgSzj (div n 10) ln
+
 -- - egy szám $b$ számrendszerbeli alakjában a $d$-vel egyenlő számjegyek számát (például a $b = 10$-es számrendszerben a $d = 2$-es számjegyek száma),
 --   Példák függvényhívásokra:
 
@@ -34,9 +85,55 @@ szjSzorzat3 n res
 --   fugv 1023 2 1 -> 10
 --   fugv 345281 16 4 -> 2
 --   ```
+
+bSzamDSzj n b d
+  | n < 0 = bSzamDSzj (abs n) b d
+  | n <b = if n ==d then 1 else 0
+  | otherwise =
+    if mod n b == d
+      then 1 + bSzamDSzj (div n b) b d
+      else bSzamDSzj (div n b) b d 
+
 -- - az 1000-ik Fibonacci számot.
 
+fibo a b res n
+  | n == 0 = res
+  | otherwise = fibo b res (res+b) (n-1)
+
+fiboN n = fibo 0 1 0 n
+
+fiboN2 n = fiboSg 0 1 0 n
+  where 
+    fiboSg _ _ res 0 = res
+    fiboSg a b res n = fiboSg b res (res + b) (n - 1)
+
+
+
 -- II. Alkalmazzuk a map függvényt a I.-nél megírt függvényekre.
+
+ls1=[234,64,12,9,0]
+
+szjSzorzatLs ls =map szjSzorzat2 ls
+
+szjSzorzatLs2 ls = map(\x -> (x,szjSzorzat2 x)) ls
+
+szjOsszegLs = map szjOsszeg ls1
+
+szjOsszegLs2 ls =map (\x -> szjOsszeg2 x 0) ls
+
+szjSzamLs ls = map szjSzama2 ls
+
+ls2=[(577723707,7),(423,3),(0,1),(12,2)]
+szjSzamOsszegLs ls = map (uncurry szjSzamOsszeg) ls
+
+szjSzamOsszegLs2 ls =map (\(x,szj) -> szjOsszeg x szj) ls
+
+legSzjLs ls =map (\x -> legSzj x 0) ls
+
+
+ls3 = [(7673573,10,7),(1024,2,1),(1023,2,1),(345281,16,4)]
+
+bSzamrDSzjLs = map(\(n,b,d) -> bSzamDSzj n b d) ls3
 
 -- **Megoldott feladatok:**
 
